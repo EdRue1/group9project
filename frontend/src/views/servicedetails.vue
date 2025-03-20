0b9c9ae8bdf059365b8066c4c7cac25d83b298e5
Eduardo Rueda <efrueda-pliego@uh.edu>
2025-03-19 20:11:04 -0500

    Refactored servicedetails.vue to Composition API

--- frontend/src/views/servicedetails.vue
+++ frontend/src/views/servicedetails.vue
@@ -1,92 +1,102 @@
-<!-- This view allows a user to update a specific service's information. -->
 <template>
   <main>
-    <!--Header-->
+    <!-- Header -->
     <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
       Service Details
     </h1>
+
     <div class="px-10 py-20">
-        <!-- grid container -->
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
-          <h2 class="text-2xl font-bold">Service Details</h2>
-          <!-- Service Name input field -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Service Name</span>
-              <span style="color: #ff0000">*</span>
-              <input type="text"
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="service.name" />
-            </label>
-            <span v-if="v$.service.name.$error" class="text-red-500">
-              Service Name is required
-            </span>
-          </div>
+      <!-- Grid container -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+        <h2 class="text-2xl font-bold">Service Details</h2>
 
-          <!-- Description input field -->
-          <div class="flex flex-col">
-            <label>
-              <span class="text-gray-700">Description:</span>
-              <textarea
-                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="service.description"></textarea>
-            </label>
-          </div>
+        <!-- Service Name input field -->
+        <div class="flex flex-col">
+          <label class="block">
+            <span class="text-gray-700">Service Name</span>
+            <span class="text-red-500">*</span>
+            <input 
+              type="text" 
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="service.name" 
+            />
+          </label>
+          <span v-if="v$.service.name.$error" class="text-red-500">
+            Service Name is required
+          </span>
         </div>
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
-          <div></div>
-          <!-- form field: update service description -->
-          <div class="flex flex-col">
-            <label class="block">
-              <span class="text-gray-700">Service Status</span>
-              <span style="color: #ff0000">*</span>
-              <div class="mt-1">
-                <label class="inline-flex items-center">
-                  <input type="radio" class="form-radio" value="Active" v-model="service.status">
-                  <span class="ml-2">Active</span>
-                </label>
-                <label class="inline-flex items-center ml-6">
-                  <input type="radio" class="form-radio" value="Inactive" v-model="service.status">
-                  <span class="ml-2">Inactive</span>
-                </label>
-              </div>
-            </label>
-          </div>
+        <!-- Description input field -->
+        <div class="flex flex-col">
+          <label>
+            <span class="text-gray-700">Description:</span>
+            <textarea 
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="service.description">
+            </textarea>
+          </label>
         </div>
+      </div>
 
-        <!-- grid container -->
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
-          <div></div>
-          <!-- Update Service button -->
-          <div class="flex justify-between mt-10 mr-20">
-            <div class="flex justify-between mt-10 mr-25">
-              <button @click="submitServiceUpdate" type="submit" class="bg-green-700 disabled:opacity-50 text-white rounded"
-                :disabled="user.role === 'viewer'">
-                Update Service
-              </button>
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+        <div></div>
+
+        <!-- Service Status -->
+        <div class="flex flex-col">
+          <label class="block">
+            <span class="text-gray-700">Service Status</span>
+            <span class="text-red-500">*</span>
+            <div class="mt-1">
+              <label class="inline-flex items-center">
+                <input type="radio" class="form-radio" value="Active" v-model="service.status">
+                <span class="ml-2">Active</span>
+              </label>
+              <label class="inline-flex items-center ml-6">
+                <input type="radio" class="form-radio" value="Inactive" v-model="service.status">
+                <span class="ml-2">Inactive</span>
+              </label>
             </div>
-          </div>
-          <!--Delete Service button-->
-          <div class="flex justify-between mt-20 mr-20">
-            <button @click="submitDeleteService" type="submit" class="bg-red-700 disabled:opacity-50 text-white rounded"
-              :disabled="user.role === 'viewer'">
-              Delete Service
-            </button>
-          </div>
-          <!--Go back button-->
-          <div class="flex justify-between mt-20 mr-20">
-            <button type="reset" class="border border-red-700 bg-white text-red-700 rounded" @click="this.$router.back()">
-              Go back
-            </button>
-          </div>
+          </label>
         </div>
+      </div>
+
+      <!-- Buttons -->
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+        <div></div>
+
+        <!-- Update Service button -->
+        <div class="flex justify-between mt-10">
+          <button 
+            @click="submitServiceUpdate" 
+            class="bg-green-700 disabled:opacity-50 text-white rounded px-4 py-2"
+            :disabled="user.role === 'viewer'">
+            Update Service
+          </button>
+        </div>
+
+        <!-- Delete Service button -->
+        <div class="flex justify-between mt-10">
+          <button 
+            @click="submitDeleteService" 
+            class="bg-red-700 disabled:opacity-50 text-white rounded px-4 py-2"
+            :disabled="user.role === 'viewer'">
+            Delete Service
+          </button>
+        </div>
+
+        <!-- Go Back button -->
+        <div class="flex justify-between mt-10">
+          <button class="border border-red-700 bg-white text-red-700 rounded px-4 py-2" @click="$router.back()">
+            Go Back
+          </button>
+        </div>
+      </div>
     </div>
 
     <hr class="mt-10 mb-10" />
-    <!-- Display Found Data -->
+
+    <!-- Display Events -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
-      <!--List of Events table-->
       <div class="ml-10">
         <h2 class="text-2xl font-bold">List of Events</h2>
         <h3 class="italic">Click table row to view event details</h3>
@@ -101,10 +111,13 @@
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
-            <tr @click="$router.push({name: 'eventdetails', params: { id: event._id } })" 
-            v-for="event in events" :key="event._id" class="cursor-pointer"
-              :class="{ 'hoverRow': hoverId === event._id }" @mouseenter="hoverId = event._id"
-              @mouseleave="hoverId = null">
+            <tr 
+              v-for="event in events" :key="event._id" 
+              class="cursor-pointer"
+              :class="{ 'hoverRow': hoverId === event._id }" 
+              @mouseenter="hoverId = event._id"
+              @mouseleave="hoverId = null"
+              @click="$router.push({name: 'eventdetails', params: { id: event._id } })">
               <td class="p-2 text-left">{{ event.name }}</td>
               <td class="p-2 text-left">{{ formatDate(event.date) }}</td>
               <td class="p-2 text-left">{{ event.address.line1 }}</td>
@@ -117,103 +130,97 @@
 </template>
 
 <script>
+import { reactive, ref, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import useVuelidate from '@vuelidate/core'
 import { required } from '@vuelidate/validators'
-import { useLoggedInUserStore } from "../store/loggedInUser";
+import { useLoggedInUserStore } from '../store/loggedInUser'
 import { getServiceById, getEventsByServiceId, updateService, deleteService } from '../api/api'
 import { useToast } from 'vue-toastification'
 
-//Notifications
-const toast = useToast()
-
 export default {
-  data() {
-    return {
-      //variable to store service information
-      service: {
-        name: null,
-        description: null,
-        status: null
-      },
-      // all events that host the service
-      events: [],
-      // variable stores the ID of the row that the mouse is currently hovering over (to highlight the row red)
-      hoverId: null,
-    }
-  },
   setup() {
-    // register Vuelidate and loggedIn store
-    const v$ = useVuelidate();
-    const user = useLoggedInUserStore();
-    return { v$, user };
-  },
-  validations() {
-    // validations      
-    return {
+    // Router & Route
+    const route = useRoute()
+    const router = useRouter()
+
+    // Store
+    const user = useLoggedInUserStore()
+
+    // Toast for notifications
+    const toast = useToast()
+
+    // Reactive state
+    const service = reactive({
+      name: null,
+      description: null,
+      status: null
+    })
+
+    const events = ref([])
+    const hoverId = ref(null)
+
+    // Vuelidate setup
+    const rules = {
       service: {
-        name: { required },
-      },
-    };
-  },
-  async mounted() {
-    // when component is mounted, data is loaded
-    try {
-      const [serviceResponse, eventsResponse] = await Promise.all([
-        getServiceById(this.$route.params.id),
-        getEventsByServiceId(this.$route.params.id),
-      ]);
-
-      this.service = serviceResponse;
-      this.events = eventsResponse;
-
-    } catch (error) {
-      toast.error(error);
+        name: { required }
+      }
     }
-  },
-  methods: {
-    // method to format the event dates
-    formatDate(date) {
-      const isoDate = new Date(date);
-      const year = isoDate.getUTCFullYear();
-      const month = String(isoDate.getUTCMonth() + 1).padStart(2, '0');
-      const day = String(isoDate.getUTCDate()).padStart(2, '0');
-      return `${month}/${day}/${year}`;
-    },
+    const v$ = useVuelidate(rules, { service })
 
-    // method called when user attempts to update the service
-    async submitServiceUpdate() {
-      // Trigger validation
-      this.v$.$validate();
-
-      if (this.v$.$error) {
-        // Form is invalid, do not proceed
-        return;
-      }
-
+    // Fetch service & events when component is mounted
+    onMounted(async () => {
       try {
-        const response = await updateService(this.$route.params.id, this.service);
-        this.$router.push('/findservice')
-        toast.success(response)
-      } catch (error) {
-        toast.error('error updating service', error)
-      }
-    },
-
-    // method to make the API call to delete the service - can only be deleted service is not used in any event
-    async submitDeleteService() {
-      try {
-        if (this.events.length != 0)
-        {
-          toast.info('Service can not be deleted since it is being used by events.')
-          return
-        }
-        const response = await deleteService(this.$route.params.id);
-        toast.success(response)
-        this.$router.push('/findservice')
+        const [serviceResponse, eventsResponse] = await Promise.all([
+          getServiceById(route.params.id),
+          getEventsByServiceId(route.params.id)
+        ])
+        Object.assign(service, serviceResponse)
+        events.value = eventsResponse
       } catch (error) {
         toast.error(error)
       }
-    },
+    })
+
+    // Format date
+    const formatDate = (date) => {
+      const isoDate = new Date(date)
+      const year = isoDate.getUTCFullYear()
+      const month = String(isoDate.getUTCMonth() + 1).padStart(2, '0')
+      const day = String(isoDate.getUTCDate()).padStart(2, '0')
+      return `${month}/${day}/${year}`
+    }
+
+    // Update service
+    const submitServiceUpdate = async () => {
+      v$.value.$validate()
+      if (v$.value.$error) return
+
+      try {
+        const response = await updateService(route.params.id, service)
+        router.push('/findservice')
+        toast.success(response)
+      } catch (error) {
+        toast.error('Error updating service')
+      }
+    }
+
+    // Delete service
+    const submitDeleteService = async () => {
+      if (events.value.length !== 0) {
+        toast.info('Service cannot be deleted since it is being used by events.')
+        return
+      }
+      try {
+        await deleteService(route.params.id)
+        toast.success('Service deleted successfully')
+        router.push('/findservice')
+      } catch (error) {
+        toast.error(error)
+      }
+    }
+
+    return { service, events, hoverId, formatDate, submitServiceUpdate, submitDeleteService, v$, user }
   }
 }
 </script>
